bbd9e7cc6d9075415528af153f99c80b68a86d76
Abua Isaac <aknessy@yahoo.com>
2025-02-06 14:59:11 +0100

    Updates the index.blade.php template for the Create Staff Function

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css2?family=figtree:400;500;600;700&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -22,80 +22,105 @@
                 </div>
             </div>
             <div class="lg:gap-x-8 gap-x-2 flex w-full py-4">
-                <div class="w-full flex flex-col lg:w-1/3">
-                    <a href="#" class="flex items-center justify-start space-x-3 py-2 px-4 text-center text-slate-500 text-xs hover:text-slate-900 hover:underline transition-all">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                            <path d="M3 17l9 5l9 -5v-3l-9 5l-9 -5v-3l9 5l9 -5v-3l-9 5l-9 -5l9 -5l5.418 3.01"></path>
-                        </svg>
-                        <span class="">Upload Bulk</span>
-                    </a>
-                    <div class="w-full flex flex-col bg-brown-50 px-4 py-4 border border-brown-100 rounded-md">
-                        <div class="mt-5 flex flex-col gap-y-3">
-                            <a href="{{ route('profile-settings') }}" class="flex items-center justify-start space-x-3 rounded-lg bg-gradient-to-tr from-slate-600 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md  focus:bg-slate-600 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 hover:border-slate-800 active:border-slate-800 focus:border-slate-800 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <span class="rounded-full w-6 h-6 bg-brown-300 text-white  p-1 flex items-center justify-center">                                
+                <form type="post" accept-charset="" action="" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="w-full flex flex-col lg:w-1/3">
+                        <a href="#" class="flex items-center justify-start space-x-3 py-2 px-4 text-center text-slate-500 text-xs hover:text-slate-900 hover:underline transition-all">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                                <path d="M3 17l9 5l9 -5v-3l-9 5l-9 -5v-3l9 5l9 -5v-3l-9 5l-9 -5l9 -5l5.418 3.01"></path>
+                            </svg>
+                            <span class="">Upload Bulk</span>
+                        </a>
+                        <div class="w-full flex flex-col bg-brown-50 px-4 py-4 border border-brown-100 rounded-md">
+                            <div class="mt-5 flex flex-col gap-y-3">
+                                <a href="{{ route('profile-settings') }}" class="flex items-center justify-start space-x-3 rounded-lg bg-gradient-to-tr from-slate-600 to-slate-700 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md  focus:bg-slate-600 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 hover:border-slate-800 active:border-slate-800 focus:border-slate-800 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                    <span class="rounded-full w-6 h-6 bg-brown-300 text-white  p-1 flex items-center justify-center">                                
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                                            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                                            <path d="M16 19h6"></path>
+                                            <path d="M19 16v6"></path>
+                                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+                                        </svg>
+                                    </span>
+                                    <span class="">Personal Information</span>
+                                </a>
+                                <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                                        <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-                                        <path d="M16 19h6"></path>
-                                        <path d="M19 16v6"></path>
-                                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+                                        <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
+                                        <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
+                                        <path d="M5 8h4"></path>
+                                        <path d="M9 16h4"></path>
+                                        <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z"></path>
+                                        <path d="M14 9l4 -1"></path>
+                                        <path d="M16 16l3.923 -.98"></path>
                                     </svg>
-                                </span>
-                                <span class="">Personal Information</span>
-                            </a>
-                            <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                                    <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
-                                    <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
-                                    <path d="M5 8h4"></path>
-                                    <path d="M9 16h4"></path>
-                                    <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z"></path>
-                                    <path d="M14 9l4 -1"></path>
-                                    <path d="M16 16l3.923 -.98"></path>
-                                  </svg>
-                                <span class="">Educational Progression</span>
-                            </a>
-                            <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                                    <path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18"></path>
-                                    <path d="M13 8l2 0"></path>
-                                    <path d="M13 12l2 0"></path>
-                                </svg>
-                                <span class="">Work/Job Progression</span>
-                            </a>
-                            <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                                    <path d="M13 12v.01"></path>
-                                    <path d="M3 21h18"></path>
-                                    <path d="M5 21v-16a2 2 0 0 1 2 -2h7.5m2.5 10.5v7.5"></path>
-                                    <path d="M14 7h7m-3 -3l3 3l-3 3"></path>
-                                  </svg>
-                                <span class="">Leave Record</span>
-                            </a>
-                            <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
-                                    <path d="M4 6l5.5 0"></path>
-                                    <path d="M4 10l5.5 0"></path>
-                                    <path d="M4 14l5.5 0"></path>
-                                    <path d="M4 18l5.5 0"></path>
-                                    <path d="M14.5 6l5.5 0"></path>
-                                    <path d="M14.5 10l5.5 0"></path>
-                                    <path d="M14.5 14l5.5 0"></path>
-                                    <path d="M14.5 18l5.5 0"></path>
-                                </svg>
-                                <span class="">Referee(s)</span>
-                            </a>
+                                    <span class="">Educational Progression</span>
+                                </a>
+                                <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                                        <path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18"></path>
+                                        <path d="M13 8l2 0"></path>
+                                        <path d="M13 12l2 0"></path>
+                                    </svg>
+                                    <span class="">Work/Job Progression</span>
+                                </a>
+                                <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                                        <path d="M13 12v.01"></path>
+                                        <path d="M3 21h18"></path>
+                                        <path d="M5 21v-16a2 2 0 0 1 2 -2h7.5m2.5 10.5v7.5"></path>
+                                        <path d="M14 7h7m-3 -3l3 3l-3 3"></path>
+                                    </svg>
+                                    <span class="">Leave Record</span>
+                                </a>
+                                <a href="#" class="flex items-center justify-start space-x-3 rounded-lg bg-transparent py-2 px-4 text-center text-sm hover:text-slate-700 transition-all disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2">
+                                        <path d="M4 6l5.5 0"></path>
+                                        <path d="M4 10l5.5 0"></path>
+                                        <path d="M4 14l5.5 0"></path>
+                                        <path d="M4 18l5.5 0"></path>
+                                        <path d="M14.5 6l5.5 0"></path>
+                                        <path d="M14.5 10l5.5 0"></path>
+                                        <path d="M14.5 14l5.5 0"></path>
+                                        <path d="M14.5 18l5.5 0"></path>
+                                    </svg>
+                                    <span class="">Referee(s)</span>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="mt-5 w-full flex fle-col">
+                            <div class="bg-brown-50 px-4 py-4 border border-brown-100 rounded-md min-w-full w-full">
+                                <div class="flex flex-col items-start border-b border-b-brown-100">
+                                    <h2 class="text-lg font-semibold mb-0 text-slate-600">Next of Kin</h2>
+                                    <p class="font-normal text-md text-slate-500 m-0">Staff Next of Kin</p>
+                                </div>
+                                <div class="block py-4">
+                                    <div class="mb-3 w-full flex flex-col">
+                                        <div class="w- flex space-x-2">
+                                            <div id="cloneable">
+                                                <label class="font-sans text-sm text-slate-800">Name</label>
+                                                <div class="relative">
+                                                    <input type="text" class="w-full pl-3 pr-10 py-2 bg-gray-50 placeholder:text-slate-400 text-slate-600 text-sm border border-gray-200 rounded-md transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 focus:shadow" name="next-of-kin[]" placeholder="NoK Name" value="" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2" class="absolute w-5 h-5 top-2.5 right-2.5 text-slate-400">
+                                                        <path d="M3 17c3.333 -3.333 5 -6 5 -8c0 -3 -1 -3 -2 -3s-2.032 1.085 -2 3c.034 2.048 1.658 4.877 2.5 6c1.5 2 2.5 2.5 3.5 1l2 -3c.333 2.667 1.333 4 3 4c.53 0 2.639 -2 3 -2c.517 0 1.517 .667 3 2"></path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                            <button type="button" class="cloneBtn text-xs text-slate rounded-md bg-gradient-to-tr from-slate-600 to-slate-700">+</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="w-full lg:w-2/3 px-4 py-2 bg-brown-50 border border-brown-100 rounded-lg">
-                    <div class="flex flex-col items-start border-b border-b-brown-100">
-                        <h2 class="text-lg font-semibold mb-0 text-slate-600">Basic Information</h2>
-                        <p class="font-normal text-md text-slate-500 m-0">Create a new staff by filling their basic information</p>
-                    </div>
-                    <div class="py-4 block">
-                        <form type="post" accept-charset="" action="" enctype="multipart/form-data">
-                            @csrf
-
+                    <div class="w-full lg:w-2/3 px-4 py-2 bg-brown-50 border border-brown-100 rounded-lg">
+                        <div class="flex flex-col items-start border-b border-b-brown-100">
+                            <h2 class="text-lg font-semibold mb-0 text-slate-600">Basic Information</h2>
+                            <p class="font-normal text-md text-slate-500 m-0">Create a new staff by filling their basic information</p>
+                        </div>
+                        <div class="py-4 block">
                             <div class="form-input-area py-3 px-3 mt-3 gap-y-4">
                                 <div class="mb-3 w-full lg:flex lg:space-x-4 space-x-2">
                                     <div class="w-full">
@@ -155,7 +180,7 @@
                                                 <path d="M8 4l2 0"></path>
                                                 <path d="M9 17l0 .01"></path>
                                                 <path d="M21 6l-2 3l2 3l-2 3l2 3"></path>
-                                              </svg>
+                                                </svg>
                                         </div>
                                     </div>
                                     <div class="w-full max-w-sm">
@@ -255,12 +280,17 @@
                                     </button>
                                 </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
 
         <x-footer appVersion="{{ $app_version }}"/>
     </x-slot>
 </x-dashboard>
+<script>
+    $(function(){
+        
+    })
+</script>
